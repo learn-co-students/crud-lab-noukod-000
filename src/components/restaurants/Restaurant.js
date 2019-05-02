d04997b7e1c5167d98579132a2e2df52843c8083
Saint-Jean Germila <germila@noukod.com>
2019-05-02 18:29:07 +0000

    Done.

--- src/components/restaurants/Restaurant.js
+++ src/components/restaurants/Restaurant.js
@@ -1,22 +1,29 @@
-import React, { Component } from 'react';
+
+import React, { Component } from 'react'
 import ReviewsContainer from '../../containers/ReviewsContainer'
 
 class Restaurant extends Component {
 
+handleOnClick = () => {
+this.props.deleteRestaurant(this.props.restaurant.id)
+}
 
-  render() {
-    const { restaurant } = this.props;
+render() {
+const {restaurant} = this.props
 
-    return (
-      <div>
-        <li>
-          {restaurant.text}
-          <button> X </button>
-          <ReviewsContainer restaurant={restaurant}/>
-        </li>
-      </div>
-    );
-  }
-};
+return (
+  this.props.restaurant.id !== '' ?
+  <div>
+    <br />
+    <li>
+      {restaurant.text}
+      <label> </label><button onClick={this.handleOnClick}>X</button>
+      <ReviewsContainer restaurant={restaurant}/>
+    </li>
+  </div> :
+  null
+)
+}
+}
 
-export default Restaurant;
+export default Restaurant
